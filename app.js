57c2fe5ed3f02890d9ad6e505cf62f55ef27e12f
Subashree <subashree.25cs@licet.ac.in>
2025-02-22 20:23:42 +0530

    rejection example

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Subashree, here goes your content'));
+app.get('/', (req, res) => res.send('This is an example for rejection'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
